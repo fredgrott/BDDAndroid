c468389619c77caf6f14524bd1a5138ade2300ac
Fred Grott <fred.grott@gmail.com>
2012-01-21 01:30:47 -0600

    first commit

---
+++ src/com/github/shareme/bddandroid/BDDAndroidActivity.java
@@ -0,0 +1,13 @@
+package com.github.shareme.bddandroid;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class BDDAndroidActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
